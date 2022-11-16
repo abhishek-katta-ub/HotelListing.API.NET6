422abb79957d8101580e29d3d6b8030f0fbffea8
abhishek-samisen <117162315+abhishek-samisen@users.noreply.github.com>
2022-11-16 10:25:49 +0530

    dddd

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,8 +1,20 @@
+using HotelListing.API.Configurations;
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +29,7 @@ builder.Services.AddCors(options =>
 
 builder.Host.UseSerilog((ctx, lc)=>lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
+builder.Services.AddAutoMapper(typeof(MapperConfig));
 
 var app = builder.Build();
 
